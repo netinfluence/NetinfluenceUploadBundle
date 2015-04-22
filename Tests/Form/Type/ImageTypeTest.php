cf92f8fde63c8fbd8c67842a22017c2a963fc745
Romaric Drigon <romaric@netinfluence.ch>
2015-04-22 10:32:00 +0200

    Added another Form test

--- Tests/Form/Type/ImageTypeTest.php
+++ Tests/Form/Type/ImageTypeTest.php
@@ -3,12 +3,13 @@
 namespace Netinfluence\UploadBundle\Tests\Form\Type;
 
 use Netinfluence\UploadBundle\Form\Type\ImageType;
+use Symfony\Component\Form\Test\TypeTestCase;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Class ImageTypeTest
  */
-class ImageTypeTest extends \PHPUnit_Framework_TestCase
+class ImageTypeTest extends TypeTestCase
 {
     /**
      * @var ImageType
@@ -17,6 +18,8 @@ class ImageTypeTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
+        parent::setUp();
+
         $this->sut = new ImageType();
     }
 
@@ -52,5 +55,39 @@ class ImageTypeTest extends \PHPUnit_Framework_TestCase
         $resolver->resolve(array(
             'data_class' => 'Netinfluence\UploadBundle\Model\TemporaryFile'
         ));
+    }
+
+    public function test_it_submits_data_to_temporary_form_file()
+    {
+        $form = $this->factory->create($this->sut);
+
+        $form->submit(array(
+            'path' => 'some/path/image.jpg',
+            'temporary' => 1,
+        ));
+
+        $this->assertTrue($form->isSynchronized());
+
+        $data = $form->getData();
+        $this->assertEquals('Netinfluence\UploadBundle\Model\FormFile', get_class($data));
+        $this->assertEquals('some/path/image.jpg', $data->getPath());
+        $this->assertEquals(true, $data->isTemporary());
+    }
+
+    public function test_it_submits_data_to_final_form_file_by_default()
+    {
+        $form = $this->factory->create($this->sut);
+
+        $form->submit(array(
+            'path' => 'some/path/image.jpg',
+            // nothing: we should use the default value
+        ));
+
+        $this->assertTrue($form->isSynchronized());
+
+        $data = $form->getData();
+        $this->assertEquals('Netinfluence\UploadBundle\Model\FormFile', get_class($data));
+        $this->assertEquals('some/path/image.jpg', $data->getPath());
+        $this->assertEquals(false, $data->isTemporary());
     }
 }
